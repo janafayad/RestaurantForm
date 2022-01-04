43190e003ddce088e528e9ba73d6865f50a4cf0a
Jana Fayad <fayad.f.jana@gmail.com>
2022-01-04 09:43:27 +0200

    4-1-22

---
+++ src/app/restaurant-dash/restaurant-dash.component.ts
@@ -0,0 +1,100 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { ApiService } from '../shared/api.service';
+import { RestaurantData } from './restaurant.model';
+
+@Component({
+  selector: 'app-restaurant-dash',
+  templateUrl: './restaurant-dash.component.html',
+  styleUrls: ['./restaurant-dash.component.css']
+})
+export class RestaurantDashComponent implements OnInit {
+
+  formValue!: FormGroup
+  allRestaurantData: any;
+  restaurantModelObj: RestaurantData = new RestaurantData;
+  showAdd!:boolean;
+  showbtn!:boolean;
+  constructor(private formBuilder: FormBuilder, private api:ApiService) { }
+
+  ngOnInit(): void {
+    this.formValue = this.formBuilder.group({
+      name: [''],
+      email: [''],
+      mobile: [''],
+      address: [''],
+      services: ['']
+    })
+    this.getAllData();
+  }
+
+  clickAddResto(){
+    this.formValue.reset();
+    this.showAdd=true;
+    this.showbtn=false;
+  }
+
+  addResto(){
+    this.restaurantModelObj.name = this.formValue.value.name;
+    this.restaurantModelObj.email = this.formValue.value.email;
+    this.restaurantModelObj.mobile = this.formValue.value.mobile;
+    this.restaurantModelObj.address = this.formValue.value.address;
+    this.restaurantModelObj.services = this.formValue.value.services;
+
+    this.api.postRestaurant(this.restaurantModelObj).subscribe(res=>{
+      console.log(res);
+      alert("Restaurant " + this.restaurantModelObj.name + " was added successfully!");
+      
+      let ref = document.getElementById('clear');
+      ref?.click();
+      this.formValue.reset();
+      this.getAllData();
+    },
+    err=>{
+       console.log(err);
+      alert("Error!");
+    })
+  }
+
+  getAllData(){
+    this.api.getRestaurant().subscribe(res => {
+      this.allRestaurantData = res;
+    })
+  }
+
+  deleteResto(data:any){
+    this.api.deleteRestaurant(data.id).subscribe(res => {
+      alert("Restaurant deleted");
+      this.getAllData();
+    });
+  }
+
+  onEditResto(data:any){
+    this.showAdd=false;
+    this.showbtn=true;
+    this.restaurantModelObj.id = data.id;
+    this.formValue.controls['name'].setValue(data.name);
+    this.formValue.controls['email'].setValue(data.email);
+    this.formValue.controls['mobile'].setValue(data.mobile);
+    this.formValue.controls['address'].setValue(data.address);
+    this.formValue.controls['services'].setValue(data.services);
+  }
+
+  updateResto(){
+    this.restaurantModelObj.name = this.formValue.value.name;
+    this.restaurantModelObj.email = this.formValue.value.email;
+    this.restaurantModelObj.mobile = this.formValue.value.mobile;
+    this.restaurantModelObj.address = this.formValue.value.address;
+    this.restaurantModelObj.services = this.formValue.value.services;
+
+    this.api.updateRestaurant(this.restaurantModelObj, this.restaurantModelObj.id)
+    .subscribe(res=>{
+      alert("Restaurant Records Updated!");
+
+      let ref = document.getElementById('clear');
+      ref?.click();
+      this.formValue.reset();
+      this.getAllData();
+    })
+  }
+}
